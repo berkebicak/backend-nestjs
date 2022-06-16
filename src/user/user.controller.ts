66098b4e1d2e669ff6d778ffd00665f42bd3b96a
berkebicak <berke.bicak@hotmail.com>
2022-06-16 10:12:59 +0300

    services update

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,36 +1,45 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  Put,
+  Delete,
+} from '@nestjs/common';
+import { UserService } from './user.service';
 import { UserCreateDto, UserUpdateDto } from 'tools/dtos/user.dto';
 import { UserModel } from 'tools/models/user.model';
-import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+  constructor(private userService: UserService) {}
 
-    constructor(private userService:UserService){}
+  @Post()
+  async createUser(@Body() body: UserCreateDto): Promise<UserModel> {
+    return await this.userService.create(body);
+  }
 
-    @Post()
-   async createUser(@Body() body: UserCreateDto): Promise<UserModel> {
-        return await this.userService.create(body);
-    }
+  @Get()
+  async getAllUsers(): Promise<UserModel[]> {
+    return await this.userService.findAll();
+  }
 
-    @Get()
-   async getAllUsers(): Promise<UserModel[]> {
-        return await this.userService.findAll();
+  @Get(':id')
+  async getUser(@Param() params): Promise<UserModel> {
+    return await this.userService.findOne(params.id);
+  }
 
-    }
+  @Put(':id')
+  async updateUser(
+    @Param('id') id: string,
+    @Body() userUpdateDto: UserUpdateDto,
+  ): Promise<UserModel> {
+    return await this.userService.update(id, userUpdateDto);
+  }
 
-    @Get(':id')
-   async getUser(@Param() params): Promise<UserModel[]> {
-        return await this.userService.findOne(params.id);
-    }
-
-    @Put(':id')
-    async updateUser(@Param('id')id: string,@Body() UserUpdateDto:UserUpdateDto, ): Promise<UserModel>{ 
-        return await this.userService.update(id, UserUpdateDto);
-    }
-
-    @Delete(':id')
-    async removeUser(@Param('id')id: string ): Promise<UserModel>{ 
-        return await this.userService.delete(id);
-    }
+  @Delete(':id')
+  async removeUser(@Param('id') id: string): Promise<UserModel> {
+    return await this.userService.delete(id);
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,78 +1,17 @@
-import { Injectable, Res } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { create } from 'domain';
-import { ResourceService } from 'libs/services/resource.service';
-import { Model } from 'mongoose';
-import { UserCreateDto, UserUpdateDto } from 'tools/dtos/user.dto';
-import { AuditModel } from 'tools/models/audit.model';
+import { Injectable } from '@nestjs/common';
 import { UserModel } from 'tools/models/user.model';
-
-const result: UserModel[] = []
+import { UserCreateDto, UserUpdateDto } from 'tools/dtos/user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { ResourceService } from 'libs/services/resource.service';
 
 @Injectable()
-export class UserService extends ResourceService<UserModel,UserCreateDto,UserUpdateDto> {
-    constructor(@InjectModel('User') userMongo: Model<UserModel>,
-    ){
-        super (userMongo)
-    }
-
-   /* getAllUsers() : UserModel[]{
-
-        if (result.length == 0) {
-            this.creatingMockUser(
-            {
-                birthDay:new Date(),
-                email:"berke.bicak@hotmail.com",
-                name:"Berke",
-                surname:"Bıçak",
-                password:"12345",
-
-            }
-            
-            )
-        }
-
-        return result;
-    }
-
-    getUserById(id) : any{
-        const user = result.find(data=>data.id == id);
-
-        if(!user){
-            return "user does not exist"
-        }
-        else{
-            return user;
-        }
-    }
-    
-
-    /*createUser(body:UserCreateDto){
-        const isExist = result.find(res=>{res.email == body.email;});
-        
-        if (isExist) {
-
-            return isExist;
-        }
-        else{
-
-            this.creatingMockUser(body);
-            return result.slice(result.length - 1, result.length);
-
-        }
-
-    }*/
-
-   /* private creatingMockUser(data:any){
-        const user: UserModel = new UserModel();
-        user.birthDay = data.birthDay;
-        user.email = data.email;
-        user.name = data.name;
-        user.surname = data.surname;
-        user.password = data.password;
-
-        user.id = (Math.floor(Math.random()*60) + 1).toString();
-        
-        result.push(user);
-    }*/
+export class UserService extends ResourceService<
+  UserModel,
+  UserCreateDto,
+  UserUpdateDto
+> {
+  constructor(@InjectModel('User') userMongo: Model<UserModel>) {
+    super(userMongo);
+  }
 }
