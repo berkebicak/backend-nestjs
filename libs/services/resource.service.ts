e6af212a918d53c98a76976f5933aeeb2d1aa87c
berkebicak <berke.bicak@hotmail.com>
2022-06-16 10:52:39 +0300

    changed

--- libs/services/resource.service.ts
+++ libs/services/resource.service.ts
@@ -1,39 +1,38 @@
-import { AuditModel } from "tools/models/audit.model";
-import { Model } from "mongoose";
+import { AuditModel } from 'tools/models/audit.model';
+import { Model } from 'mongoose';
 
+export class ResourceService<T extends any, C extends any, U extends any> {
+  constructor(protected readonly mongoModel: Model<T>) {}
 
-export class ResourceService<T extends any, C extends any, U extends any>{
-    
-    constructor(protected readonly mongoModel:Model<T>){}
-    
-    async create(model: C): Promise<T> {
+  async create(model: C): Promise<T> {
+    const audit = new AuditModel();
+    audit.active = true;
+    audit.createdBy = 'Admin';
+    audit.createdDate = new Date();
 
-        const audit = new AuditModel();
-        audit.active = true;
-        audit.createdBy = 'Admin'
-        audit.createdDate = new Date();
+    const createdModel = new this.mongoModel({ ...model, ...audit });
 
-        const createdUser = new this.mongoModel({...model as {} , ...audit});
+    return await createdModel.save();
+  }
 
-        return await createdUser.save();
-    }
-
-   async findAll(): Promise<T[]>{           //data sayısını belli sizelara bölücez.
+  async findAll(): Promise<T[]> {
     return await this.mongoModel.find().exec();
-   }
+  }
 
-   async findOne(id: string): Promise<T[]>{          
-    return await this.mongoModel.find({_id: id}).exec();
-   }
+  async findOne(id: string): Promise<T> {
+    return await this.mongoModel.find({ _id: id }).exec();
+  }
 
-   async delete(id:string): Promise<T>{
-    return await this.mongoModel.findByIdAndRemove({_id: id}).exec();
-   }
+  async delete(id: string): Promise<T> {
+    return await this.mongoModel.findByIdAndRemove({ _id: id }).exec();
+  }
 
-   async update(id:string, dto: U): Promise<T>{
-   let newModel = this.mongoModel.findOne({_id: id}).exec();
-    newModel = {...newModel, ...dto as {}};
-    return await this.mongoModel.findByIdAndUpdate(id, newModel, {new: true}).exec();
+  async update(id: string, dto: U): Promise<T> {
+    let newModel = this.mongoModel.findOne({ _id: id }).exec();
+    newModel = { ...newModel, ...dto };
 
-   }
+    return await this.mongoModel
+      .findByIdAndUpdate(id, newModel, { new: true })
+      .exec();
+  }
 }
